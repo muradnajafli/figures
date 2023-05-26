42a8a3436adb2fa8c5b66ab675a3c5799bea6c90
Murad Najafli <muradnecefli05@gmail.com>
2023-05-26 16:32:11 +0400

    Update8 FiguresTask

--- src/main/java/com/epam/rd/autotasks/figures/Circle.java
+++ src/main/java/com/epam/rd/autotasks/figures/Circle.java
@@ -26,6 +26,6 @@ class Circle extends Figure {
     }
     @Override
     public String toString() {
-        return "(" + center.getX() + "," + center.getY() + ")" + radius;
+        return getClass().getSimpleName() + "[(" + center.getX() + "," + center.getY() + ")" + radius + "]";
     }
 }
